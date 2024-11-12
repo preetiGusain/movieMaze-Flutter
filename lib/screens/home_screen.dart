8ecef9c472e3bbd54a7efd5be129680e92a51ae5
Preeti Gusain <preetigusain9173@gmail.com>
2024-11-12 16:16:03 +0530

    Added movie thumbnails

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,17 +22,37 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('MovieMaze'),
-        ),
-        body: movies.isEmpty
-            ? const Center(child: CircularProgressIndicator()) 
-            :ListView.builder(
+      appBar: AppBar(
+        title: const Text('MovieMaze'),
+      ),
+      body: movies.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
               itemCount: movies.length,
               itemBuilder: (context, index) {
                 final movie = movies[index]['show'];
-                final name = movie['name'];
+                final name = movie != null && movie['name'] != null ? movie['name'] : 'No Title';
+                final imageUrl = movie != null && movie['image'] != null ? movie['image']['medium'] : 'No Image';
+                
+                
                 return ListTile(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  leading: Container(
+                    width: 60.0,
+                    height: 90.0,
+                    decoration: BoxDecoration(
+                      image: imageUrl != null
+                          ? DecorationImage(
+                              image: NetworkImage(imageUrl),
+                              fit: BoxFit.cover,
+                            )
+                          : null,
+                      color: Colors.grey[300],
+                    ),
+                    child: imageUrl == null 
+                        ? const Icon(Icons.movie, color: Colors.white)
+                        : null,
+                  ),
                   title: Text(name),
                 );
               },
