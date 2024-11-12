ae619a0bfbd161c4f0acd926bbfd612e2f65f7ef
Preeti Gusain <preetigusain9173@gmail.com>
2024-11-12 21:14:08 +0530

    Added logic for displaying search item

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,7 +15,47 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //Get the search query passed from the HomeScreen
+    final searchTerm = ModalRoute.of(context)?.settings.arguments as String;
+    fetchSearchResults(searchTerm);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Results for "$searchTerm"'),
+      ),
+      body: searchResults.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+            itemCount: searchResults.length,
+            itemBuilder: (context, index) {
+              final movie = searchResults[index]['show'];
+              final name = movie['name'] ?? 'No Title';
+              final imageUrl = movie['image']?['medium'] ?? 'No Image';
+              final summary = movie['summary'] ?? 'No summary available';
+
+
+              return ListTile(
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 8.0, horizontal: 16.0),
+                leading: Container(
+                  width: 60.0,
+                  height: 90.0,
+                  decoration: BoxDecoration(
+                    image: imageUrl != null
+                        ? DecorationImage(
+                          image: NetworkImage(imageUrl),
+                          fit: BoxFit.cover,
+                          )
+                        : null,
+                    color: Colors.grey[300],
+                  ),
+                  child: imageUrl == null
+                      ? const Icon(Icons.movie, color: Colors.white)
+                      : null,
+                ),
+              );
+            })
+    );
   }
 
   void fetchSearchResults(String searchTerm) async {
